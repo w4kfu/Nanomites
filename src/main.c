809d970bcd4d439d20928963b25da44b1c577467
Samuel CHEVET <gw4kfu@gmail.com>
2012-01-23 15:54:31 +0100

    Adding necessary stuff for debugging child process

--- src/main.c
+++ src/main.c
@@ -1,25 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <sys/ptrace.h>
+#include <sys/user.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-
-void sig_trap(int sig)
+int	son_work(void)
 {
-  sig = sig;
-  printf("SIG TRAP !\n");
+  printf("Hello ! I'm the son :]\n");
+  __asm__("int3");
+  return (0);
 }
 
-int main(void)
+void	print_regsinfo(pid_t son)
 {
+  struct user_regs_struct regs;
 
-  if (signal(SIGTRAP, sig_trap) == SIG_ERR)
+  ptrace (PTRACE_GETREGS, son, 0, &regs);
+  printf("Regs.eip = %08lx\n", regs.eip);
+
+}
+
+int	father_work(pid_t son)
+{
+  int	status;
+
+  /* ptrace (PTRACE_SETOPTIONS, son, 0, PTRACE_O_TRACESYSGOOD); */
+  while (1)
     {
-      perror("signal()");
-      exit(EXIT_FAILURE);
+      waitpid(son, &status, 0);
+      if (WIFSIGNALED (status))
+	{
+	  printf("WTF\n");
+	  ptrace (PTRACE_KILL, son, 0, 0);
+	}
+      if (WIFSTOPPED (status) && WSTOPSIG (status))
+	{
+	  printf("int3 ?\n");
+	  print_regsinfo(son);
+	  ptrace(PTRACE_CONT, son, 0, 0);
+	}
+      if (WIFEXITED (status))
+	return (0);
     }
-  __asm__
-    (
-     "int3"
-     );
+}
+
+int	main(void)
+{
+  pid_t son;
+
+  if ((son = fork()) == -1)
+    {
+      perror("[-] fork()");
+      exit (EXIT_FAILURE);
+    }
+  if (son == 0)
+    {
+      if (ptrace(PT_TRACE_ME, 0, 0, 0) < 0)
+	{
+	  perror("[-] ptrace()");
+	  exit(EXIT_FAILURE);
+	}
+      son_work();
+    }
+  else
+    {
+      father_work(son);
+    }
   return (0);
 }
