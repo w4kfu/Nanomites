5bac1477285807c8c19287c6d31c33aafcba7a94
Samuel CHEVET <gw4kfu@gmail.com>
2012-04-10 17:39:30 -0500

    Fixing problem with ZFLAG

--- crackme/test.c
+++ crackme/test.c
@@ -14,12 +14,12 @@
 # define SF_MASK 1 << 7 // bit 7
 # define OF_MASK 1 << 11 // bit 11
 
-# define CF_SET(eflags) ((eflags & CF_MASK) == 1)
-# define PF_SET(eflags) ((eflags & PF_MASK) == 1)
-# define AF_SET(eflags) ((eflags & AF_MASK) == 1)
-# define ZF_SET(eflags) ((eflags & ZF_MASK) == 1)
-# define SF_SET(eflags) ((eflags & SF_MASK) == 1)
-# define OF_SET(eflags) ((eflags & OF_MASK) == 1)
+# define CF_SET(eflags) ((eflags & CF_MASK) & 1)
+# define PF_SET(eflags) ((eflags & PF_MASK) & 1)
+# define AF_SET(eflags) ((eflags & AF_MASK) & 1)
+# define ZF_SET(eflags) ((eflags >> 6) & 1)
+# define SF_SET(eflags) ((eflags & SF_MASK) & 1)
+# define OF_SET(eflags) ((eflags & OF_MASK) & 1)
 
 extern void son_work(void);
 
@@ -68,10 +68,12 @@ void callback_sigtrap(pid_t pid)
   switch (type_j)
     {
     case 1:
+      printf("EFLAGS = %x\n", regs.eflags);
       printf("JZ and JE\n");
-      regs.eip += dest + 1;
-      printf("New eip = %08X\n", regs.eip);
-      regs_write(pid, &regs);
+      if ((regs.eflags >> 6) & 1)
+	regs.eip += dest + 1;
+      else
+	regs.eip += 5;
       break;
     case 2:
       printf("JNZ and JNE\n");
@@ -84,14 +86,63 @@ void callback_sigtrap(pid_t pid)
       break;
     case 5:
       printf("JMP !\n");
+      regs.eip += dest + 1;
       break;
     default:
       printf("WTF !\n");
       break;
     }
 
-
+  printf("New eip = %08X\n", regs.eip);
+  regs_write(pid, &regs);
   cont_signal(pid, 0);
+}
+
+long mem_read(pid_t pid, unsigned addr)
+{
+  ptrace(PTRACE_PEEKDATA, pid, addr, 0);
+}
+
+
+void mem_print_chars(pid_t pid, unsigned addr)
+{
+  unsigned data;
+
+  data = mem_read(pid, addr);
+  fprintf(stderr, "@%8x : %02x %02x %02x %02x\n", addr,
+          data % 0x100, (data >> 8) % 0x100,
+          (data >> 16) % 0x100, (data >> 24) % 0x100);
+}
+
+void regs_dump(pid_t pid)
+{
+  struct user_regs_struct regs;
+  if (regs_read(pid, &regs) == -1)
+    fprintf(stderr, "Erreur regs_read !\n");
+  fprintf(stderr, "eax=%08x ebx=%08x ecx=%08x edx=%08x\n",
+          regs.eax, regs.ebx, regs.ecx, regs.edx);
+  fprintf(stderr, "esi=%08x edi=%08x\n", regs.esi, regs.edi);
+  fprintf(stderr, "eip=%08x esp=%08x ebp=%08x\n",
+          regs.eip, regs.esp, regs.ebp);
+}
+
+
+void callback_generic(pid_t pid, int signal)
+{
+  unsigned addr;
+  struct user_regs_struct regs;
+
+  regs_read(pid, &regs);
+  addr = regs.eip;
+  if (signal == SIGILL)
+    printf("Illegal instruction @%x\n", addr);
+  else if (signal == SIGSEGV)
+    printf("Segmentation fault @%x\n", addr);
+  else
+    printf("signal %d @%x\n", signal, addr);
+  mem_print_chars(pid, addr);
+  regs_dump(pid);
+  exit(1);
 }
 
 void debug_loop(pid_t pid)
@@ -104,12 +155,11 @@ void debug_loop(pid_t pid)
       waitpid(pid, &status, 0);
       if (WIFSTOPPED(status))
         {
-          /* signal ptrace */
           signal = WSTOPSIG(status);
           if (signal == SIGTRAP)
 	    callback_sigtrap(pid);
-          /* else */
-          /*   callback_generic(pid, signal); */
+          else
+	    callback_generic(pid, signal);
         }
       else
         {
@@ -134,14 +184,9 @@ int main(void)
   else
     {
       if (activate_debug() == -1)
-	{
-	  exit(2);
-	}
+       	exit(2);
       else
-	{
-	  son_work();
-	}
+	son_work();
     }
-
   return (0);
 }
